17caa5811c9fdd9f726890c84d4269a79437cabe
Mareta Fernanda <56830674+maretafernanda@users.noreply.github.com>
2025-09-06 21:03:47 +0700

    Change

--- Fundamental SQL Using FUNCTION and GROUP BY.sql
+++ Fundamental SQL Using FUNCTION and GROUP BY.sql
@@ -48,5 +48,80 @@ FROM students;
 # MOD() + EXP()
 SELECT StudentID, FirstName, LastName, MOD(Semester1,2) AS Semester1, Semester2, EXP(MarkGrowth) FROM students;
 
---------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------
 # 2. Fungsi Skalar Text
+# Fungsi - fungsi skalar text di dokumentasi
+# postgresql: https://www.postgresql.org/docs/9.1/functions-string.html
+# mysql: https://dev.mysql.com/doc/refman/8.0/en/string-functions.html
+# 2. a. Fungsi Text - CONCAT()
+# SELECT CONCAT(ColumnName1, ColumnName2, ColumnNameN)  
+# FROM TableName; 
+SELECT StudentID, CONCAT(FirstName, LastName) as Name, Semester1, Semester2, MarkGrowth
+FROM students;
+
+# 2. b. Fungsi Text - SUBSTRING_INDEX() -> Mengambil pecahan text pertama atau sub-text sebelum delimiter. 
+# SELECT SUBSTRING_INDEX(column, delimiter, index to return)  
+# FROM TableName; 
+SELECT StudentID, SUBSTRING_INDEX(Email,'@',1) as Name
+FROM students;
+
+# 2. c. Fungsi Text - SUBSTR() -> Mengambil substring mulai pada karakter ke-N hingga karakter ke-N
+# SELECT SUBSTR(columnName, Start Index, Number of string to be extract)
+# FROM TableName;
+SELECT StudentID, SUBSTR(FirstName, 2, 3) as Initial
+FROM students;
+
+# 2. d. Fungsi Text - LENGTH() -> Menghitung panjang karakter
+# SELECT LENGTH(ColumnName)
+# FROM TableName; 
+SELECT StudentID, FirstName, LENGTH(FirstName) as Total_Char
+FROM students;
+
+# 2. e. Fungsi Text - REPLACE() -> Mengganti data karakter
+# SELECT REPLACE(ColumnName, Character/String to be change, New String/Character)
+# FROM TableName; 
+SELECT StudentID, Email, REPLACE(Email, 'yahoo', 'gmail') as New_Email
+FROM students;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------
+# --- TUGAS PRAKTEK ---
+
+# Gunakan fungsi UPPER() untuk mengubah kolom FirstName menjadi seluruhnya kapital, 
+# dan gunakan LOWER() untuk mengubah kolom LastName menjadi seluruhnya non-kapital. 
+# Gunakan kedua fungsi tersebut dalam satu SELECT-Statement.
+
+# JAWABAN: 
+    SELECT StudentID, UPPER(FirstName) as FirstName, LOWER(LastName) as LastName FROM students;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------
+# 3. Fungsi Aggregate
+# Fungsi aggregate digunakan untuk melakukan perhitungan pada sekelompok nilai.
+
+# 3. a. Fungsi Aggregate - SUM()
+# SELECT SUM(ColumnName)  
+# FROM TableName; 
+SELECT SUM(Semester1) as Total_1, SUM(Semester2) as Total_2 FROM students;
+
+# 3. b. Fungsi Aggregate - COUNT()
+# SELECT COUNT(ColumnName)  
+# FROM TableName; 
+SELECT COUNT(FirstName) as Total_Student FROM students;
+
+# 3. c. Fungsi Aggregate - AVG()
+# SELECT AVG(ColumnName)  
+# FROM TableName; 
+SELECT AVG(Semester1) as AVG_1, AVG(Semester2) as AVG_2 
+FROM students;
+
+-------------------------------------------------------------------
+# --- TUGAS PRAKTEK ---
+
+# Gunakan fungsi MIN() dan MAX() untuk menghitung nilai dari kolom Semester1 dan Semester2.
+# Gunakan kedua fungsi tersebut dalam satu SELECT-Statement.
+
+# JAWABAN: 
+    SELECT MIN(Semester1) as Min1, MAX(Semester1) as Max1, MIN(Semester2) as Min2, MAX(Semester2) as Max2 FROM students;
+
+-------------------------------------------------------------------
+# 3. d. Fungsi GROUP BY()
+# GROUP BY statement akan mengelompokkan data yang bernilai sama ke dalam satu group, dan dengan menggunakan fungsi aggregate seperti (COUNT, MAX, MIN, SUM, AVG)
